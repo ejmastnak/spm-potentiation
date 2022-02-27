205b6959dba6422321f73161676bc1b4bcf15fd2
Elijan Mastnak <ejmastnak@gmail.com>
2022-02-26 21:20:22 -0500

    Add function to autogenerate SPM subplot figure
    Motivation: Frontiers requires that sub-figures be submitted as a single
    figure (instead of e.g. combining individual figures with the `subcaption`
    package in LaTeX).
    This commit creates a function to autogenerate an 8-axis subfigure, of
    SPM test results of which would be prohibitively time-consuming to
    repeatedly put together by hand.

--- src/frontiers.py
+++ src/frontiers.py
@@ -10,6 +10,8 @@ import preprocessing
 import plotting
 import constants
 
+plt.style.use('frontiers.mplstyle')
+
 """
 Collection of functions intended specifically for use in preparing
 the potentiation paper for the Frontiers journal.
@@ -164,7 +166,8 @@ def tmp_srdjan_set_analysis(pre_data_dir, post_data_dir,
     """
     For temporary analysis of normalized CSV files sent by Srdjan 
     for which I am uncertain how the normalization was performed.
-    Output analogous to `normalized_analysis_by_set`.
+    Output analogous to `normalized_analysis_by_set`:
+
     """
 
     # Perform SPM analysis on each set of normalized measurements
@@ -186,45 +189,126 @@ def tmp_srdjan_set_analysis(pre_data_dir, post_data_dir,
         # In practice only zero-th row is skipped.
         tmg_data_start_row = constants.TMG_DATA_START_ROW_FOR_SPM
         fig_format = "jpg"
-        fig_dpi = 600
+        fig_dpi = 500
 
-        spm_plot_output_file = spm_plot_output_dir + "set{}-plot.{}".format(s + 1, fig_format)
+        spm_plot_output_file = spm_plot_output_dir + "spm-set{}.{}".format(s + 1, fig_format)
         plotting.plot_spm_ttest(t, ti, pre_data, post_data,
                 tmg_data_start_row, spm_plot_output_file,
                 fig_format=fig_format, fig_dpi=fig_dpi,
                 show_plot=False, save_figures=True)
 
-        fig_format = "png"
-        fig_dpi = 600
-        spm_plot_output_file = spm_plot_output_dir + "set{}-plot.{}".format(s + 1, fig_format)
-        plotting.plot_spm_ttest(t, ti, pre_data, post_data,
-                tmg_data_start_row, spm_plot_output_file,
-                fig_format=fig_format, fig_dpi=fig_dpi,
-                show_plot=False, save_figures=True)
 
-        fig_format = "svg"
-        fig_dpi = 600
-        spm_plot_output_file = spm_plot_output_dir + "set{}-plot.{}".format(s + 1, fig_format)
-        plotting.plot_spm_ttest(t, ti, pre_data, post_data,
-                tmg_data_start_row, spm_plot_output_file,
-                fig_format=fig_format, fig_dpi=fig_dpi,
-                show_plot=False, save_figures=True)
+def tmp_make_spm_plot_for_article(pre_data_dir, post_data_dir, 
+        points_per_msmnt=100, max_sets=4, skiprows=1,
+        save_figure=True, show_plot=False):
+    """
+    Function used for the sole purpose of a generating a 4-axis plot showing
+    SPM results for sets 1, 2, 3, and 4 for use in the Frontiers article.
 
-        fig_format = "eps"
-        fig_dpi = 600
-        spm_plot_output_file = spm_plot_output_dir + "set{}-plot.{}".format(s + 1, fig_format)
-        plotting.plot_spm_ttest(t, ti, pre_data, post_data,
-                tmg_data_start_row, spm_plot_output_file,
-                fig_format=fig_format, fig_dpi=fig_dpi,
-                show_plot=False, save_figures=True)
+    Currently uses normalized CSV files sent by Srdjan 
+    for which I am uncertain how the normalization was performed.
+    """
+    fig, axes = plt.subplots(max_sets, 2, figsize=(6.8, 8))
+    tmg_data_start_row = constants.TMG_DATA_START_ROW_FOR_SPM
+    output_dir = "/home/ej/Documents/projects/tmg-bmc/frontiers-2022/article/figures/"
+    output_file = output_dir + "spm-plot.jpg"
 
-        fig_format = "pdf"
-        fig_dpi = 600
-        spm_plot_output_file = spm_plot_output_dir + "set{}-plot.{}".format(s + 1, fig_format)
-        plotting.plot_spm_ttest(t, ti, pre_data, post_data,
-                tmg_data_start_row, spm_plot_output_file,
-                fig_format=fig_format, fig_dpi=fig_dpi,
-                show_plot=False, save_figures=True)
+    subfib_labels = ["(1)", "(2)", "(3)", "(4)"]
+
+    fig_dpi = 500
+
+    pre_color = plotting.pre_color
+    post_color = plotting.post_color
+    post_color2 = plotting.post_color2
+    pre_alpha = plotting.pre_alpha
+    post_alpha = plotting.post_alpha
+
+    tline_color = plotting.tline_color
+    tfill_color = plotting.tfill_color
+    tfill_color2 = plotting.tfill_color2
+
+    linewidth = 1.5
+
+    # Perform SPM analysis on each set of normalized measurements
+    for s in range(max_sets):
+        pre_data = np.loadtxt(pre_data_dir + "{}.csv".format(s + 1), delimiter=',',
+                    skiprows=skiprows, max_rows=points_per_msmnt)
+        post_data = np.loadtxt(post_data_dir + "{}.csv".format(s + 1), delimiter=',',
+                    skiprows=skiprows, max_rows=points_per_msmnt)
+        pre_data, post_data = preprocessing.fix_false_spm_significance(pre_data, post_data)
+        t, ti = spm_analysis.get_spm_ti(pre_data, post_data)
+
+        # BEGIN PLOTTING
+        # --------------------------------------------- #
+        N = np.shape(post_data)[0]       # number of rows in pre/post_data
+        time = np.linspace(0, N - 1, N)  # [ms] assuming 1 kHz sampling
+
+        post_mean = np.mean(post_data, axis=1)
+        pre_mean = np.mean(pre_data, axis=1)
+        post_sd = np.std(post_data, ddof=1, axis=1)
+        pre_sd = np.std(pre_data, ddof=1, axis=1)
+
+        # Plot TMG measurement
+        # --------------------------------------------- #
+        ax = axes[s, 0]
+        plotting.remove_spines(ax)
+
+        # Only put x label on bottom axis to save vertical space
+        if s == max_sets - 1:  
+            ax.set_xlabel("Time [ms]")
+        ax.set_ylabel("Normalized displacement")
+
+        # Mean value of time-series measurements
+        ax.plot(time, pre_mean, color=pre_color, linewidth=linewidth, 
+                label="Pre-ISQ mean", zorder=4)
+        ax.plot(time, post_mean, color=post_color, linewidth=linewidth,
+                label="Post-ISQ mean", zorder=3)
+
+        # Standard deviation clouds
+        ax.fill_between(time, post_mean - post_sd, post_mean + post_sd, 
+                color=post_color, alpha=post_alpha, zorder=2)
+        ax.fill_between(time, pre_mean - pre_sd, pre_mean + pre_sd,
+                color=pre_color, alpha=pre_alpha, zorder=1)
+
+        ax.text(-0.30, 0.5, subfib_labels[s], transform=ax.transAxes, fontsize=12)
+        ax.legend(framealpha=1.0)
+        # --------------------------------------------- #
+
+        # Plot SPM results
+        # --------------------------------------------- #
+        ax = axes[s, 1]
+        plotting.remove_spines(ax)
+
+        # Only put x label on bottom axis to save vertical space
+        if s == max_sets - 1:  
+            ax.set_xlabel("Time [ms]")
+        ax.set_ylabel("SPM $t$-statistic", labelpad=-0.1)
+
+        # Plot SPM t-statistic
+        ax.plot(time, t.z, color=tline_color)  # plot t-curve
+
+        # Plot dashed line at y = 0
+        ax.axhline(y=0, color='black', linestyle=':')  
+
+        # Plot dashed line at SPM significance threshold
+        ax.axhline(y=ti.zstar, color='#000000', linestyle='--')
+
+        # Text box showing alpha, threshold value, and p value
+        ax.text(73, ti.zstar + 1.0, plotting.get_annotation_text(ti),
+                va='bottom', ha='left', 
+                bbox=dict(facecolor='#FFFFFF', edgecolor='#222222', boxstyle='round,pad=0.3'))
+
+        # Shade between curve and threshold
+        ax.fill_between(time, t.z, ti.zstar, where=t.z >= ti.zstar,
+                interpolate=True, color=tfill_color)
+
+    plt.tight_layout()
+
+    if save_figure:
+        plt.savefig(output_file, dpi=fig_dpi, bbox_inches='tight', pad_inches = 0)
+
+    if show_plot:
+        plt.show()
 
 
 def get_pre_and_post_exercise_data_tensors(pre_data_dir, post_data_dir,
@@ -351,8 +435,15 @@ def tmp_srdjan_set_analysis_wrapper():
     max_sets = 4
     skiprows = 1
 
-    tmp_srdjan_set_analysis(pre_data_dir, post_data_dir, 
-            spm_param_output_dir, spm_plot_output_dir,
+    
+    # Generate SPM params and plots for each set
+    # tmp_srdjan_set_analysis(pre_data_dir, post_data_dir, 
+    #         spm_param_output_dir, spm_plot_output_dir,
+    #         points_per_msmnt=points_per_msmnt,
+    #         max_sets=max_sets, skiprows=2)
+
+    # Generate one figure holding SPM plots for all sets
+    tmp_make_spm_plot_for_article(pre_data_dir, post_data_dir, 
             points_per_msmnt=points_per_msmnt,
             max_sets=max_sets, skiprows=2)
 
