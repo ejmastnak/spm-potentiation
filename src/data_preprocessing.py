deb5994b45cd976bf3e4af1b5940f20978c70e54
Elijan Mastnak <ejmastnak@gmail.com>
2022-03-11 13:17:04 -0500

    Implement SPM preprocessing
    - Removing filter-induced significance region at signal start
    - Normalization to unit amplitude for cross-subject comparison

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,4 +1,7 @@
 import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import constants, frontiers_utils
 import xlsx_csv_conversion
 
@@ -23,7 +26,7 @@ For a longer description of the processed dataset, see the file
 
 """
 
-def frontiers_conversion_wrapper():
+def process_raw_excel_files():
     """
     The first step in the data processing pipeline;
     used to process raw Excel TMG measurements.
@@ -81,20 +84,115 @@ def frontiers_conversion_wrapper():
 
 
 # SPM pre-processing
-"""
-Processes data
-Input: `/data/csv-processed/`
-Output: `/data/csv-for-spm-normed/`
+def prepare_csv_files_for_spm():
+    """
+    Processes data
+    Input: `/data/csv-processed/`
+    Output: `/data/csv-for-spm-normed/`
 
-File structure similar to `/data/csv-processed/`, i.e.
-- One pre-ISQ and one post-ISQ CSV file per subject
-- Each file contains four columns; these columns hold the first pre-ISQ or post-ISQ measurement from each of the subject's first four measurement sets.
-Differences:
-- Meaurements
+    File structure similar to `/data/csv-processed/`, i.e.
+    - One pre-ISQ and one post-ISQ CSV file per subject
+    - Each file contains four columns; these columns hold the first pre-ISQ or post-ISQ measurement from each of the subject's first four measurement sets.
+    Differences:
+    - Meaurements
 
-"""
+    """
+    skiprows = constants.TMG_ROWS_TO_SKIP_FOR_SPM
+    nrows = constants.TMG_ROWS_TO_USE_FOR_SPM
 
+    pre_input_dir = constants.INITIAL_DATA_DIR + "pre-exercise/"
+    post_input_dir = constants.INITIAL_DATA_DIR + "post-exercise/"
+    pre_filenames = []
+    post_filenames = []
+
+    for filename in frontiers_utils.natural_sort(os.listdir(pre_input_dir)):
+        pre_filenames.append(filename)
+    for filename in frontiers_utils.natural_sort(os.listdir(post_input_dir)):
+        post_filenames.append(filename)
+
+    pre_output_dir = constants.SPM_DATA_DIR + "pre-exercise/"
+    post_output_dir = constants.SPM_DATA_DIR + "post-exercise/"
+    pre_normed_output_dir = constants.NORMED_SPM_DATA_DIR + "pre-exercise/"
+    post_normed_output_dir = constants.NORMED_SPM_DATA_DIR + "post-exercise/"
+
+    for i in range(len(pre_filenames)):
+        pre_df = pd.read_csv(pre_input_dir + pre_filenames[i],
+                sep=',', header=0, nrows=nrows)
+        post_df = pd.read_csv(post_input_dir + post_filenames[i],
+                sep=',', header=0, nrows=nrows)
+        pre_column_headers = pre_df.columns.values.tolist()
+        post_column_headers = post_df.columns.values.tolist()
+
+        pre_df, post_df = remove_spm_significance_from_filter_artefact(pre_df, post_df)
+
+        # Drop first row and save files to CSV
+        pre_df.iloc[skiprows:].to_csv(pre_output_dir + pre_filenames[i], index=False)
+        post_df.iloc[skiprows:].to_csv(post_output_dir + post_filenames[i], index=False)
+
+        # Normalize files and save again
+        # --------------------------------------------- #
+
+        # Convert to Numpy for easier processing
+        pre_np = pre_df.to_numpy()
+        post_np = post_df.to_numpy()
+
+        pre_max = pre_np.max(axis=0)
+        post_max = post_np.max(axis=0)
+        # Example: If pre_max = [1 2 42] and post_max = [3, 4, 0]
+        #          then  pre_post_max = [3, 4, 42]
+        pre_post_max = np.maximum(pre_max, post_max)
+
+        pre_df = pre_df/pre_post_max
+        post_df = post_df/pre_post_max
+
+        # Drop first row and save files to CSV
+        pre_df.iloc[skiprows:].to_csv(pre_normed_output_dir + pre_filenames[i], header=pre_column_headers, index=False)
+        post_df.iloc[skiprows:].to_csv(post_normed_output_dir + post_filenames[i], header=post_column_headers, index=False)
+
+
+def remove_spm_significance_from_filter_artefact(pre_df, post_df, 
+        first_n_rows_to_average=constants.FILTER_ARTEFACT_ROWS_TO_AVERAGE):
+    """
+    Context: Initial IIR filtering applied to TMG signals creates a small
+             filter artefact over the first few milliseconds of a TMG signal.
+             This artefact can lead to non-physical regions of significance
+             in the first few ms of an SPM t-test between TMG signals.
+    This function removes this SPM significance very simply, namely by 
+    subtracting from the post-exercise data the average of the difference 
+    between pre- and post-exercise data over the first few ms of 
+    the TMG curve; see implementation details in the code below.
+    This is safe to do without appreciable affecting the later 
+    (i.e. t > ~5 ms) portion of a TMG signal because signal values in the 
+    first few ms are of the order 0.001 mm, while typical values later in
+    the TMG curve are of the order 5 mm.
+
+    Parameters
+    ----------
+    pre_df : DataFrame
+        Pandas DataFrame containing raw pre-exericse data.
+        Rows traverse time and columns traverse measurements
+    post_df : DataFrame
+        Pandas DataFrame containing raw pre-exericse data.
+        Rows traverse time and columns traverse measurements
+
+    Returns
+    -------
+    pre_df : DataFrame
+        Same as pre_df; see processing description above.
+    post_df : DataFrame
+        Processed analog of post_df; see processing description above.
+    
+    """
+    # Mean is taken over rows (time) and then over columns (measurements)
+    # to produce a single scalar value for each 2D DataFrame.
+    pre_mean = pre_df.head(first_n_rows_to_average).mean().mean()
+    post_mean = post_df.head(first_n_rows_to_average).mean().mean()
+
+    if post_mean > pre_mean:
+        post_df = post_df - np.mean(post_mean - pre_mean)
+
+    return pre_df, post_df
 
 if __name__ == "__main__":
-    frontiers_conversion_wrapper()
-
+    # process_raw_excel_files()
+    prepare_csv_files_for_spm()
