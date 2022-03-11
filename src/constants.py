da853631f11c9cc6f60af43a7a6b9fb20c36b538
Elijan Mastnak <ejmastnak@gmail.com>
2022-03-11 13:18:22 -0500

    Remove unused constants

--- src/constants.py
+++ src/constants.py
@@ -13,21 +13,20 @@ TMG_PARAM_STATS_BY_SET_DIR = ROOT_OUTPUT_DIR + "tmg-param-stats-by-set/"
 TMG_PARAM_STATS_RELTO_SET1_DIR = ROOT_OUTPUT_DIR + "tmg-param-stats-relto-set1/"
 
 # --------------------------------------------- #
-# CSV data file row at which to begin reading data (0-indexed)
-start_row = 1  
+# The number of rows to skip from the beginning of a raw TMG signal
+# when performing SPM analysis.
+# Motivation: the first row of a TMG signal is always zero,
+# which causes zero-variance problems in SPM.
+TMG_ROWS_TO_SKIP_FOR_SPM = 1
 
-# The first row, assuming zero-based indexing, of TMG data to use for SPM analysis.
-# Motivation: zero-th row is always zero, which causes zero-variance problems in SPM.
-TMG_DATA_START_ROW_FOR_SPM = 1
+# Number of rows in a TMG signal to use for SPM analysis.
+# Motivation: the full 1000 rows are not needed; most "interesting"
+# information occurs in the first 100 ms or so.
+TMG_ROWS_TO_USE_FOR_SPM = 150
 
-# Number of rows to read after start_row is reached
-max_rows = 100  
-
-# IDK, probably an offset to add to a TMG displacement signal
-displacement_offset = 0.0
-
-# Whether to normalize a TMG such that its maximum value occurs at one
-normalize = False
+# Used in `data_preprocessing.py` to prevent SPM from registering a false
+# significance region as a result of signal artefacts from filtering TMG signals.
+FILTER_ARTEFACT_ROWS_TO_AVERAGE = 3
 
 SPM_PARAM_NAMES = ["alpha",
         "threshold",
